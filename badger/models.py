6ef66d6175e51c253a4279656566d864fcc0101d
John Borwick <johnb@heitmgt.com>
2013-09-16 13:54:07 -0400

    Use AUTH_USER_MODEL rather than User explicitly

--- badger/models.py
+++ badger/models.py
@@ -20,7 +20,9 @@ from django.core.mail import send_mail
 from django.core.exceptions import ValidationError
 from django.core.files.storage import FileSystemStorage
 from django.core.files.base import ContentFile
-from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth.models import AnonymousUser
+User = settings.AUTH_USER_MODEL
+
 from django.contrib.sites.models import Site
 from django.contrib.contenttypes.models import ContentType
 
